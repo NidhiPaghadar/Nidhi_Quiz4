511277f9f1d767772803c1b0cd59d016a8ba4a1e
Nidhi Paghadar <106948288+NidhiPaghadar@users.noreply.github.com>
2023-04-14 16:56:55 -0400

    quick fix

--- Nidhi_Quiz4/Program.cs
+++ Nidhi_Quiz4/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Database connected");
+Console.WriteLine("Database access has been added to your web app.");
